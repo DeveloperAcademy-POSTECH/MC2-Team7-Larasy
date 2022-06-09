288a86f818c3ec7f5c695e123770cfb10b8c1961
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-09 19:12:12 +0900

    Larasy first commit

---
+++ Recorder/RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecorderApp.swift
+//  Recorder
+//
+//  Created by 최윤석 on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
