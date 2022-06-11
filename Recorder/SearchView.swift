adf4ab5710193123fd9c3c8ac93b0a27f8a382cf
Jiwon <zest1923@gmail.com>
2022-06-12 07:13:52 +0900

    feat: 음악 검색 결과 뿌리는 기능
    가수명, 노래 제목만 출력됨.

--- Recorder/SearchView.swift
+++ Recorder/SearchView.swift
@@ -10,7 +10,8 @@ import UIKit
 
 struct SearchView: View {
     
-    @State private var list = (1111...1121).map{String($0) }
+    @StateObject var viewModel = ViewModel()
+    @State var search = ""
     
     init() { UITableView.appearance().backgroundColor = UIColor.clear }
     
@@ -20,10 +21,27 @@ struct SearchView: View {
                 .frame(width: 300)
                 .font(.system(size: 24, weight: .bold))
             
-            SearchBar()
+            //search bar
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.white)
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    TextField("기록하고 싶은 음악, 가수를 입력하세요", text: $search)
+                        .onSubmit {
+                            viewModel.getSearchResults(search: search)
+                        }
+                }
+                .foregroundColor(.gray)
+                .padding(13)
+            }
+            .frame(height: 40)
+            .cornerRadius(10)
+            .padding(20)
             
+            // result view
             List {
-                ForEach(self.list, id: \.self) { num in
+                ForEach(viewModel.musicList, id: \.self) { music in
                     HStack {
                         Image(systemName: "photo")
                             .cornerRadius(5)
@@ -31,12 +49,14 @@ struct SearchView: View {
                             .overlay(RoundedRectangle(cornerRadius: 10)
                                 .stroke(Color.gray, lineWidth: 4))
                         
-                        VStack {
-                            Text(num)
+                        VStack(alignment: .leading) {
+                            Text(music.trackName)
                                 .font(.system(size: 17, weight: .bold))
                                 .padding(6)
-                            Text(num)
+                                .lineLimit(1)
+                            Text(music.artistName)
                                 .font(.system(size: 17))
+                                .padding(6)
                         }
                     }
                 }
@@ -45,30 +65,10 @@ struct SearchView: View {
             .listStyle(.sidebar)
         }
     }
-    
 }
 
 
-struct SearchBar: View {
-    
-    @State var search = ""
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.white)
-            HStack {
-                Image(systemName: "magnifyingglass")
-                TextField("기록하고 싶은 음악, 가수를 입력하세요", text: $search)
-            }
-            .foregroundColor(.gray)
-            .padding(13)
-        }
-        .frame(height: 40)
-        .cornerRadius(10)
-        .padding(20)
-        
-    }
-}
+
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
