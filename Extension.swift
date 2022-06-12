3db811daa35e2cc5cbfdfd696b52440cc7754c09
Jiwon <zest1923@gmail.com>
2022-06-12 17:06:34 +0900

    feat: Add color extension
    background, text1,2,3,4 추가
    속성처럼 사용 가능합니다.

---
+++ Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Extension.swift
+//  Recorder
+//
+//  Created by 이지원 on 2022/06/12.
+//
+
+import SwiftUI
+
+extension Color {
+    static let text1 = Color("text1")
+    static let text2 = Color("text2")
+    static let text3 = Color("text3")
+    static let text4 = Color("text4")
+    static let background = Color("background")
+}
