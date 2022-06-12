5174de63fe203339a54bed586f42d7506380cea9
Eunbi-Cho <moomb70@gmail.com>
2022-06-13 03:17:50 +0900

    practice

---
+++ Recorder/CdListView/CdListView/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  CdListView
+//
+//  Created by 조은비 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct Album: Identifiable {
+    var id = UUID().uuidString
+    var albumImage = String().self
+}
