dee6299f8e217eeb619107d8371bbad3422c71ba
Jiwon <zest1923@gmail.com>
2022-06-12 18:06:04 +0900

    feat: Color asset

--- Extension.swift
+++ Extension.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 extension Color {
-    static let text1 = Color("text1")
-    static let text2 = Color("text2")
-    static let text3 = Color("text3")
-    static let text4 = Color("text4")
+    // 피그마에 등록된 색상 보시고, 사용하시면 됩니다!
+    // foreground(.black) 이런식으로 사용하시면 돼요!
+    
+    //title 색상
+    static let black = Color("black")
+    static let darkgray = Color("darkgray")
+    static let gray = Color("gray")
+    static let lightgray = Color("lightgray")
+    
+    //point color
     static let blue = Color("blue")
     static let orange = Color("orange")
     static let yellow = Color("yellow")
+    
+    //etc
     static let background = Color("background")
+    static let cdplayer = Color("cdplayer")
 }
