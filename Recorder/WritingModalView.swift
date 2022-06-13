8fbfa95569c673c6251700ec2190ead78f0de538
mxnxxii <m990508m@gmail.com>
2022-06-13 22:46:40 +0900

    Feat : TextEditor Placeholder 추가

--- Recorder/WritingModalView.swift
+++ Recorder/WritingModalView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct WritingModalView: View {
+    
+    @State var placeholderText : String = "이 음악과 관련된 짧은 이야기를\n기록해보세요 (글자수 200자 제한)"
+    @State var content : String = ""
+    
     var body: some View {
         ZStack {
             Color("black")
@@ -18,39 +22,45 @@ struct WritingModalView: View {
                 HStack {
                     Text("나의 음악 이야기")
                         .foregroundColor(.white)
-                        .font(.title2)
                     Spacer()
                     Image(systemName: "xmark")
                         .imageScale(.large)
                         .foregroundColor(.white)
-
-                }
+                    
+                } // HStack - 나의 음악 이야기 & x 버튼
                 .padding(.horizontal, 41.0)
-                
+
                 VStack(alignment: .trailing, spacing: 220) {
-                    Text("이 음악과 관련된 짧은 이야기를\n기록해보세요 (글자수 200자 제한)")
-                    .font(.body)
-                    .foregroundColor(Color.titleGray)
-                    .frame(width: 256, alignment: .topLeading)
-                    //.lineSpacing(25)
-
-                    Text("완료")
-                    .font(.subheadline)
-                    .foregroundColor(Color.titleGray)
-                    //.lineSpacing(22)
+                    ZStack {
+                        if self.content.isEmpty {
+                            TextEditor (text: $placeholderText)
+                                .font(.body)
+                                .foregroundColor(.titleBlack)
+                                .disabled(true)
+                                .padding()
+                        }
+                        TextEditor (text: $content)
+                            .font(.body)
+                            .opacity(self.content.isEmpty ? 0.25 :1)
+                            .padding()
+                        } //ZStack - PlaceHolder
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 30)
+                    .frame(width: 308, height: 350)
+                    .background(Color.white)
                 }
-                .padding(.leading, 25)
-                .padding(.trailing, 19)
-                .padding(.top, 38)
-                .padding(.bottom, 20)
-                .frame(width: 308, height: 350)
-                .background(Color.white)
-                
-
+                ZStack {
+                    Text("완료")
+                        .font(.subheadline)
+                        .foregroundColor(Color.titleGray)
+                }
             }
-        }
+        
+        
     }
 }
+}
+
 
 struct WritingModalView_Previews: PreviewProvider {
     static var previews: some View {
