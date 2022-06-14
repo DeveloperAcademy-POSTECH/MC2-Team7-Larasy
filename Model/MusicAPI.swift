463ecd7ad68f20b10424bad2a2730a3a7f7721f8
jiwon lee <zest1923@gmail.com>
2022-06-14 15:41:38 +0900

    Comment: 음악 API 주석 추가

--- Model/MusicAPI.swift
+++ Model/MusicAPI.swift
@@ -8,33 +8,39 @@
 
 import SwiftUI
 
+// MARK: 음악 API 불러오기
 class MusicAPI: ObservableObject {
     @Published var musicList: [Music] = []
     
     func getSearchResults(search: String) {
-        
+
         self.musicList = []
-        
+
+        // URLSession의 싱글톤 객체
         guard var urlComponents = URLComponents(string: "https://itunes.apple.com/kr/search") else { return }
-        
         urlComponents.query = "media=music&entity=song&term=\(search)"
-        
         guard let url = urlComponents.url else { return }
         
+        // Networking 시작
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let e = error {
                 NSLog("error: \(e.localizedDescription)")
                 return
             }
             
+            // 값 받아오기
             DispatchQueue.main.async() {
                 do {
+                    
+                    // JSON Parsing
                     let object = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
                     
                     guard let jsonObject = object else { return }
                     
                     let searchResults = jsonObject["results"] as! [NSDictionary]
                     searchResults.forEach { result in
+                        
+                        // Music 객체에 저장
                         let searchResult = Music(artist: result["artistName"] as! String,
                                                  title: result["trackName"] as! String,
                                                  albumArt: result["artworkUrl100"] as! String)
@@ -45,13 +51,16 @@ class MusicAPI: ObservableObject {
                 } catch let e as NSError {
                     print("error: \(e.localizedDescription)")
                 }
-            }
-        }
+                
+            } // DispatchQueue End
+            
+        } // task End
         task.resume()
     }
 }
 
 
+// MARK: 음악 정보 저장 구조체
 struct Music: Hashable, Codable {
     var artist: String // 가수
     var title: String // 제목
