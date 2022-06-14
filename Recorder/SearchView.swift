7e5fdb7843b5bff765ef9c62977137740b620ac3
jiwon lee <zest1923@gmail.com>
2022-06-14 15:45:51 +0900

    Comment: SearchView 주석 추가

--- Recorder/SearchView.swift
+++ Recorder/SearchView.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 import UIKit
 
+
+// MA 음악 검색 View
 struct SearchView: View {
     
     @StateObject var musicAPI = MusicAPI()
     @State var search = ""
     private let placeholer = "기록하고 싶은 음악, 가수를 입력하세요"
     
-    init() { UITableView.appearance().backgroundColor = UIColor.clear } // 리스트 배경색 설정
+    init() { UITableView.appearance().backgroundColor = UIColor.clear } // 검색 결과 출력 리스트 배경색 초기화
     
     var body: some View {
         
         ZStack {
-            Color("background")
+            Color("background") // 배경색 설정
                 .ignoresSafeArea()
             
             VStack(alignment: .leading) {
-                Text("오랫동안 간직하고 싶은 나만의 음악을 알려주세요")
+                Text("오랫동안 간직하고 싶은 나만의 음악을 알려주세요") // 최상단 title 설정
                     .frame(width: 250)
                     .font(.customTitle2())
                     .lineSpacing(7)
@@ -31,33 +33,39 @@ struct SearchView: View {
                     .padding(.top, 40)
                     .padding(.leading, 20)
                 
-                SearchBar
+                SearchBar // 중앙 서치바,
                 
-                ResultView
-            }
-        }
+                ResultView // 하단 검색 결과 출력 리스트
+                
+            } //VStack End
+            
+        } // ZStack End
         .navigationBarTitle("음악 선택", displayMode: .inline)
-    }
+        
+    } // body View End
     
     
-    // 검색 바
+    
+    //MARK: - 서치바
     var SearchBar: some View {
+        
         ZStack {
-            Rectangle()
+            Rectangle() // 서치바 배경
                 .foregroundColor(.titleLightgray)
             
             HStack {
-                Image(systemName: "magnifyingglass")
+                Image(systemName: "magnifyingglass") // 돋보기 Symbol
                 
-                TextField(placeholer, text: $search)
+                TextField(placeholer, text: $search) // 입력창
                     .foregroundColor(.titleDarkgray)
                     .font(.customBody2())
-                    .onSubmit {
+                    .onSubmit { // keyboard Return Button 클릭 시
                         musicAPI.getSearchResults(search: search) // 음악 API 불러오기
                     }
                 
-                if search != "" { // 지우기 버튼 활성화
-                    Image(systemName: "xmark.circle.fill")
+                
+                if search != "" { // X 버튼 활성화
+                    Image(systemName: "xmark.circle.fill") // x버튼 이미지
                         .imageScale(.medium)
                         .foregroundColor(Color(.systemGray3))
                         .padding(3)
@@ -67,29 +75,37 @@ struct SearchView: View {
                             }
                         }
                 }
-            }
+            } // HStack End
             .foregroundColor(.titleDarkgray)
             .padding(13)
             
-        }
+        } // 배경색 ZStack End
         .frame(height: 40)
         .cornerRadius(10)
         .padding(10)
-    }
+        
+    } // SearchBar View End
     
     
-    // 검색 결과 리스트
+    
+    // MARK: - 검색 결과 리스트
     var ResultView: some View {
         
         GeometryReader { geometry in
+            
+            // 검색 결과 리스트
             List {
                 ForEach(musicAPI.musicList, id: \.self) { music in
                     
+                    // 앨범 커버, 제목, 가수 출력
                     HStack {
                         URLImage(urlString: music.albumArt) // 앨범커버
                             .cornerRadius(5)
                         
+                        // 글 작성 페이지로 전환
                         NavigationLink(destination: TestSoiView(music: music)) { // 현재 임시뷰로 연결, 추후 작성뷰로 전환
+                            
+                            // 제목, 가수 출력
                             VStack(alignment: .leading) {
                                 Text(music.title) // 노래제목
                                     .font(.customHeadline())
@@ -102,22 +118,28 @@ struct SearchView: View {
                             }
                             .padding(.leading, 10)
                             .foregroundColor(.titleDarkgray)
-                        }
+                        } // Navigation Link End
+                        
                         .buttonStyle(PlainButtonStyle())
-                    }
+                    } // HStack End
                 }
                 .listRowBackground(Color.background)
                 .listRowSeparator(.hidden)
                 .padding([.bottom, .top], 10)
                 .padding([.leading], -20)
-            }
+                
+            } // List End
             .onAppear { UITableView.appearance().contentInset.top = -35 }
-        }
-    }
-}
+            
+        } // GeometryReder End
+        
+    } // ResultView View End
+    
+} //SearchView struct End
 
 
-// 앨범 커버 불러오기
+
+// MARK: - 앨범 커버 불러오기
 struct URLImage: View {
     
     @State var data: Data?
@@ -125,15 +147,18 @@ struct URLImage: View {
     
     var body: some View {
         
-        if let data = data, let uiimage = UIImage(data: data) {
-            Image(uiImage: uiimage)
+        // 앨범 커버 불러오기
+        if let data = data, let uiimage = UIImage(data: data) { // 이미지 불러오기 성공
+            
+            Image(uiImage: uiimage) // URL Image
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 55, height: 55)
                 .background(.gray)
             
-        } else {
-            Rectangle()
+        } else { // 이미지 불러오기 실패
+            
+            Rectangle() // 회색 박스
                 .foregroundColor(.titleLightgray)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 55, height: 55)
@@ -153,6 +178,8 @@ struct URLImage: View {
     }
 }
 
+
+// MARK: - 현재 뷰 프리뷰
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
