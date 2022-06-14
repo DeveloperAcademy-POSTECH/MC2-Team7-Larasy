4fde4ea6cecb34158427b3258e7c19ea622f4bb9
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-14 22:06:58 +0900

    Feat: Write View 생성

--- Recorder/WriteView.swift
+++ Recorder/WriteView.swift
@@ -8,11 +8,73 @@
 import SwiftUI
 
 struct WriteView: View {
+    @State var isSelected : Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .font(Font.customTitle1())
-    }
-}
+        NavigationView {
+            ZStack {
+                Color.background.edgesIgnoringSafeArea(.all)
+                RecordBackground()
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("일이삼사오육칠팔구십일이삼사오육") // TODO: "music.title"
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.titleBlack)
+                            .multilineTextAlignment(.leading)
+                            .padding(.bottom, 3)
+                        
+                        Text("일이삼사오육칠팔구십일이삼사오육칠팔구십일") // TODO: "music.artist"
+                            .font(.body)
+                            .fontWeight(.regular)
+                            .foregroundColor(.titleDarkgray)
+                        Spacer()
+                    }// MusicInform VStack End
+                    .padding(.leading, 5)
+                    Spacer()
+                } // MusicImform HStack End
+                .padding(.leading, 23)
+                VStack {
+                    
+                    HStack(alignment: .center) {
+                        
+                        Button(action: {}, // TODO: action 내에 클릭시 모달을 통해 이미지 띄우는 기능 추가 예정
+                               label: {
+                            Image("PhotoComp") // 이미지 삽입
+                                .padding()
+                        }).offset(y: -70)
+                        Spacer()
+                        
+                        Button(action: {}, label: { // TODO: CD돌아가는 애니메이션 출력..?
+                            Image("CdPlayer")
+                                .padding(.trailing, 20.0)
+                        })
+                    }.offset(y: 110)
+                        .padding()
+                    
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        
+                        Button(action: {}, label: { // TODO: action 내에 클릭시 모달을 통해 StoryView 추가 예정
+                            Image("StoryComp") // 스토리 입력
+                        }).offset(x: 20,y: -50)
+                        
+                        Button(action: {}, label: { // TODO: action 내에 클릭시 모달을 통해 이미지 띄우는 기능 추가 예정
+                            Image("LylicComp") // 가사 입력
+                        }).offset(y: -30)
+                    }.offset(y: -20)
+                    Spacer()
+                    
+                }
+            }// 본문 ZStack End
+                .navigationBarItems(leading: NavigationLink(destination: SearchView(), label: {
+                    Image(systemName: "chevron.backward")
+                    Text("Music")
+                }))
+            
+        } // Navigation View 출력
+    } // View End
+} // RecordResultView End
 
 struct WriteView_Previews: PreviewProvider {
     static var previews: some View {
