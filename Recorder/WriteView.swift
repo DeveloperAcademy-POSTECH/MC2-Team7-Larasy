0c9b344ee4346150d2b7924cd80a498d56c402bd
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-14 21:40:13 +0900

    Feat : Write View 파일 추가

---
+++ Recorder/WriteView.swift
@@ -0,0 +1,21 @@
+//
+//  WriteView.swift
+//  Recorder
+//
+//  Created by 최윤석 on 2022/06/13.
+//
+
+import SwiftUI
+
+struct WriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(Font.customTitle1())
+    }
+}
+
+struct WriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteView()
+    }
+}
