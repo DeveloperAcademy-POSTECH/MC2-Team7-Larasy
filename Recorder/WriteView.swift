373248a83cc46639cfb7d0d49498818fe57025ca
YunSeok-Choi <dbstjr9898@naver.com>
2022-06-15 12:57:09 +0900

    Feat : 사용자 가사 적는 기능 추가

--- Recorder/WriteView.swift
+++ Recorder/WriteView.swift
@@ -11,6 +11,7 @@ struct WriteView: View {
     @State private var image: Image?                        // 선택된 사진, 어떤 이미지인지
     @State private var showingImagePicker = false   // 클릭됐는지 확인
     @State private var inputImage: UIImage?
+    @State private var lyrics = ""
     
     var body: some View {
         NavigationView {
@@ -39,29 +40,32 @@ struct WriteView: View {
                 VStack {
                     
                     HStack(alignment: .center) {
-                        
-                        ZStack{ // TODO: action 내에 클릭시 모달을 통해 이미지 띄우는 기능 추가 예정
-                            
-                            Image("PhotoComp") // 이미지 삽입
+                        //MARK: - // 클릭시 모달을 통해 이미지 띄우는 부분
+                        ZStack{
+                            Image("PhotoComp")
                                 .padding()
                             
                             Image(systemName: "photo")
                                 .foregroundColor(.white)
                                 .offset(y: -15)
                             
-                            image?
+                            image?                       // 사용자가 가져온 이미지를 보여주는 부분
                                 .resizable()
                                 .frame(width: 95, height: 105)
                                 .scaleEffect()
                                 .offset(y: -15)
                             
                         }
-//                        .frame(width: 150, height: 120)
                         .onTapGesture {
                             showingImagePicker = true
                         }
                         .offset(y: -70)
                         .zIndex(1)
+                        .onChange(of: inputImage) { _ in loadImage() }
+                            .sheet(isPresented: $showingImagePicker) {
+                                ImagePicker(image: $inputImage)
+                            }
+                        //MARK: - 이미지 가져오는 부분 끝
                         
                         Spacer()
                         
@@ -79,16 +83,32 @@ struct WriteView: View {
                             Image("StoryComp") // 스토리 입력
                         }).offset(x: 20,y: -50)
                         
-                        Button(action: {}, label: { // TODO: action 내에 클릭시 모달을 통해 이미지 띄우는 기능 추가 예정
+                        //MARK: - 가사 입력 받는 부분
+                        ZStack {
                             Image("LylicComp") // 가사 입력
-                        }).offset(y: -30)
+                                .offset(y: -30)
+                            HStack{
+                                
+                                if lyrics == "" {
+                                    Image(systemName: "music.mic")
+                                        .offset(x: 35)
+                                        .foregroundColor(.titleGray)
+                                }
+                                TextField("기억하고 싶은 가사",         // 가사 입력하는 텍스트 필드
+                                          text: $lyrics)
+                                .font(Font.customBody1())
+                                .lineLimit(1)
+                                .multilineTextAlignment(.center)
+                                .frame(width: 200,alignment: .center)
+                            }
+                            .offset(y: -30)
+                        }
+                        //MARK: - 가사 입력 받는 ZStack 끝
                     }.offset(y: -20)
+                    
                     Spacer()
                     
-                }.onChange(of: inputImage) { _ in loadImage() }
-                    .sheet(isPresented: $showingImagePicker) {
-                        ImagePicker(image: $inputImage)
-                    }
+                }
             }// 본문 ZStack End
             .navigationBarItems(leading: NavigationLink(destination: SearchView(), label: {
                 Image(systemName: "chevron.backward")
