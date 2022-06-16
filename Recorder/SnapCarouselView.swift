45c5b196d0a5c0de2ca28f7382ef966c74b145b0
Eunbi-Cho <moomb70@gmail.com>
2022-06-16 15:20:52 +0900

    feat: 앨범 이미지 크기 수정

--- Recorder/SnapCarouselView.swift
+++ Recorder/SnapCarouselView.swift
@@ -17,17 +17,17 @@ struct SnapCarousel: View {
         
         // 기록물 id, 곡제목, 가수명, 앨범커버 배열 예시
         let cds = [
-            Cd(id: 0, musicTitle: "노래제목1", singer: "가수명1", image: "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/f7/68/9c/f7689ce3-6d41-60cd-62d2-57a91ddf5b9d/196922067341_Cover.jpg/100x100bb.jpg"),
-            Cd(id: 1, musicTitle: "노래제목2", singer: "가수명2", image: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/17/ff/63/17ff63de-3aba-0f2d-63e7-50d66f900ebb/21UMGIM43558.rgb.jpg/100x100bb.jpg"),
-            Cd(id: 2, musicTitle: "노래제목3", singer: "가수명3", image: "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/f7/68/9c/f7689ce3-6d41-60cd-62d2-57a91ddf5b9d/196922067341_Cover.jpg/100x100bb.jpg"),
-            Cd(id: 3, musicTitle: "노래제목4", singer: "가수명4", image: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/17/ff/63/17ff63de-3aba-0f2d-63e7-50d66f900ebb/21UMGIM43558.rgb.jpg/100x100bb.jpg"),
-            Cd(id: 4, musicTitle: "노래제목5", singer: "가수명5", image: "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/f7/68/9c/f7689ce3-6d41-60cd-62d2-57a91ddf5b9d/196922067341_Cover.jpg/100x100bb.jpg")
+            Cd(id: 0, musicTitle: "노래제목1", singer: "가수명1", image: "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/f7/68/9c/f7689ce3-6d41-60cd-62d2-57a91ddf5b9d/196922067341_Cover.jpg/500x500bb.jpg"),
+            Cd(id: 1, musicTitle: "노래제목2", singer: "가수명2", image: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/17/ff/63/17ff63de-3aba-0f2d-63e7-50d66f900ebb/21UMGIM43558.rgb.jpg/500x500bb.jpg"),
+            Cd(id: 2, musicTitle: "노래제목3", singer: "가수명3", image: "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/f7/68/9c/f7689ce3-6d41-60cd-62d2-57a91ddf5b9d/196922067341_Cover.jpg/500x500bb.jpg"),
+            Cd(id: 3, musicTitle: "노래제목4", singer: "가수명4", image: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/17/ff/63/17ff63de-3aba-0f2d-63e7-50d66f900ebb/21UMGIM43558.rgb.jpg/500x500bb.jpg"),
+            Cd(id: 4, musicTitle: "노래제목5", singer: "가수명5", image: "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/f7/68/9c/f7689ce3-6d41-60cd-62d2-57a91ddf5b9d/196922067341_Cover.jpg/500x500bb.jpg")
         ]
         
         // https://gist.github.com/xtabbas/97b44b854e1315384b7d1d5ccce20623.js 의 샘플코드를 참고했습니다.
         return Canvas {
             ZStack {
-                Image("listViewBack")
+                Color("background")
                     .ignoresSafeArea()
                 // Carousel 슬라이더 기능
                 // ForEach로 items마다 Item() 뷰를 각각 불러옴
@@ -44,6 +44,7 @@ struct SnapCarousel: View {
                                 widthOfHiddenCds: widthOfHiddenCds,
                                 cdHeight: cdHeight
                             ) {
+                                // 가운데 cd만 글이 보이게 함
                                 VStack {
                                     if (content.id == UIState.activeCard) {
                                         Text("\(content.musicTitle)")
@@ -58,6 +59,7 @@ struct SnapCarousel: View {
                                         Spacer()
                                             .frame(height: 70)
                                     }
+                                    // cd자체가 버튼으로 기능해 선택된 id값 저장
                                     Button(action: {
                                         if self.showCd {
                                             self.showCd = false
@@ -78,6 +80,7 @@ struct SnapCarousel: View {
                                             Circle()
                                                 .frame(width: 40, height: 40)
                                                 .foregroundColor(.background)
+                                                .shadow(color: .gray, radius:5, x: -3, y: -3)
                                         }
                                     } // 버튼
                                     .disabled(UIState.activeCard != content.id)
@@ -125,9 +128,8 @@ struct SnapCarousel: View {
                                             timeCount()
                                         }
                                 }
-                                
-                            }
-                        }
+                            } // else문
+                        } // V스택
                         .padding(.bottom, 180)
                         .padding(.leading, 2) // CdPlayer를 그림자 포함해서 뽑아서 전체 CdPlayer와 정렬 맞추기 위함
                         
@@ -152,11 +154,11 @@ struct SnapCarousel: View {
                 } // V스택
                 .ignoresSafeArea()
             } // Z스택
-            //                } // if문
         } // Canvas
         .navigationBarTitle("List", displayMode: .inline)
     } // 바디 뷰
     
+    // 네비게이션 링크로 이동되는 RecordResultView를 딜레이시키고 애니메이션을 보여주기 위한 타임 카운터
     func timeCount() {
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
             self.value += 1
@@ -168,8 +170,9 @@ struct SnapCarousel: View {
                 return
             }
         }
-    }
-}
+    } // func
+    
+} // SnapCarousel 뷰
 
 extension UINavigationController {
     override open func viewDidLoad() {
