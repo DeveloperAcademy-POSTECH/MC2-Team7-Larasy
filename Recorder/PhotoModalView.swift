f2222c751e3b4bf5273b36f00c43f1dd4f6f994b
mxnxxii <m990508m@gmail.com>
2022-06-16 14:55:49 +0900

    Feat : Story & Photo (본문) Modal 기능 구현

---
+++ Recorder/PhotoModalView.swift
@@ -0,0 +1,66 @@
+//
+//  PhotoModalView.swift
+//  Recorder
+//
+//  Created by MINJI on 2022/06/16.
+//
+
+import SwiftUI
+
+struct PhotoModalView: View {
+    
+    @Environment(\.presentationMode) var presentation
+
+    
+    var body: some View {
+        ZStack {
+            Color.titleBlack //Background Color
+                .opacity(0.95)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                // 나의 음악 사진 & x 버튼 시작
+                HStack {
+                    Text("나의 음악 사진") // 상단 나의 음악 사진 Text 텍스트 출력
+                        .foregroundColor(.white)
+                        .font(.customTitle2())
+                    Spacer()
+                    
+                    Button(action: { // 상단 X 버튼
+                        presentation.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                    } // 상단 X 버튼 끝
+                    
+                    
+                }
+                .padding(.horizontal, 48.0)
+                // 나의 음악 사진 & x 버튼 시작 끝
+                
+                // 본문 Frame & Photo 시작
+                ZStack{
+                    Rectangle() // 본문 Background
+                        .frame(minWidth: 0, maxWidth: 308, minHeight: 0, maxHeight: 435)
+                        .foregroundColor(.white)
+                    
+                    Image("MC2") // Photo Image
+                        .resizable()
+                        .scaleEffect()
+                        .frame(minWidth: 0, maxWidth: 276, minHeight: 0, maxHeight: 314)
+                        .padding(.top, 16)
+                        .padding(.bottom, 103)
+
+                }
+            } // 본문 Frame & Photo 끝
+        }
+    }
+}
+
+struct PhotoModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoModalView()
+    }
+}
---
+++ Recorder/StoryModalView.swift
@@ -0,0 +1,66 @@
+//
+//  StoryModalView.swift
+//  Recorder
+//
+//  Created by MINJI on 2022/06/15.
+//
+
+import SwiftUI
+
+struct StoryModalView: View {
+    
+    @Environment(\.presentationMode) var presentation
+    @State var content : String = ""
+
+    
+    var body: some View {
+        ZStack {
+            Color.titleBlack //Background Color
+                .opacity(0.95)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                // 나의 음악 이야기 & x 버튼 시작
+                HStack {
+                    Text("나의 음악 이야기") // 상단 나의 음악 이야기 Text 텍스트 출력
+                        .foregroundColor(.white)
+                        .font(.customTitle2())
+                    Spacer()
+                    
+                    Button(action: { // 상단 X 버튼
+                        presentation.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                    } // 상단 X 버튼 끝
+                    
+                    
+                }
+                .padding(.horizontal, 48.0)
+                // 나의 음악 이야기 & x 버튼 시작 끝
+                
+                // 본문 Frame & Text 시작
+                ZStack{
+                    Rectangle() // 본문 Background
+                        .frame(minWidth: 0, maxWidth: 308, minHeight: 0, maxHeight: 350)
+                        .foregroundColor(.white)
+
+                    Text("본문뷰에서 보여지는 모달뷰") // 본문내용
+                        .foregroundColor(.titleDarkgray)
+                        .font(.customBody1())
+                        .frame(width:256, alignment: .leading)
+
+                }
+            } // 본문 Frame & Text 끝
+        }
+    }
+}
+
+struct StoryModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryModalView()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
