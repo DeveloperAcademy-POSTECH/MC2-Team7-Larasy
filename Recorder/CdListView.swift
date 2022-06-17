a390c7ae4d3be33273fcb5017f8425041b19fff1
jiwon lee <zest1923@gmail.com>
2022-06-17 23:49:17 +0900

    Fix: data가 없을 때, 앱이 종료되는 오류 해결

--- Recorder/CdListView.swift
+++ Recorder/CdListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CdListView: View {
     
     //coredata 관련 변수
+    let persistenceController = PersistenceController.shared
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Content.date, ascending: true)],
@@ -17,25 +18,27 @@ struct CdListView: View {
     private var items: FetchedResults<Content>
     
     var body: some View {
+        
             ZStack {
                 Color("background")
                     .ignoresSafeArea()
                 VStack {
                     //TODO: nil일때 보여주는 화면 작성
-    //                if(item == nil) {
-                    SnapCarousel().environmentObject(UIStateModel())
                     
-                    //else {
-    //                Text("아직 기록된 음악이 없어요")
-    //                    .font(Font.customTitle2())
-    //                    .foregroundColor(.titleGray)
-    //                    .padding(.bottom, 12)
-    //                    .padding(.top, 260)
-    //                Button("음악 기록하러 가기") {
-    //                    //go to WriteView
-    //                }
-    //                Spacer()
-    //                Image("ListViewCdPlayer")
+                    if items.count != 0 {
+                        SnapCarousel().environmentObject(UIStateModel())
+                    } else {
+                        Text("아직 기록된 음악이 없어요")
+                            .font(Font.customTitle2())
+                            .foregroundColor(.titleGray)
+                            .padding(.bottom, 12)
+                            .padding(.top, 260)
+                        Button("음악 기록하러 가기") {
+                            //go to WriteView
+                        }
+                    }
+                    Spacer()
+                    Image("ListViewCdPlayer")
                 }.ignoresSafeArea()
             }
             .navigationBarTitle("List", displayMode: .inline)
