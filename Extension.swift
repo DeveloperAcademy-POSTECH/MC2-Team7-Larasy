85ee08ac348efe72b7f3e5ebc6944f6e4dbabd6f
JIWON1923 <zest1923@gmail.com>
2022-09-21 23:08:51 +0900

    [Feat] #34 - UIScreen 사이즈 계산 함수

--- Extension.swift
+++ Extension.swift
@@ -73,3 +73,18 @@ extension View {
         }
     }
 }
+
+extension UIScreen {
+    
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+
+    static func getWidth(_ width: CGFloat) -> CGFloat {
+        screenWidth / 390 * width
+    }
+
+    static func getHeight(_ height: CGFloat) -> CGFloat {
+        screenHeight / 844 * height
+    }
+}
